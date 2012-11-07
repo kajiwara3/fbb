3a2bbf64a68399b29eee443d85269a5f311334d2
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-07 15:03:49 +0900

    add fbb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbb::Application.config.secret_token = '21094e75ba314dceebb171783fac3bd0dbe06079fb69386bece37d24759d3dc6a72565fd80aa8e80df8a2b0c43bcfa784c90eb2d42eb9a704d4a03850f4e0dfb'
