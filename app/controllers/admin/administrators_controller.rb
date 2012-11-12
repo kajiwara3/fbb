2f4324ab5136e8da4e25504394f988bfc90b4b4e
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-12 16:15:33 +0900

    add administrators_controller

---
+++ app/controllers/admin/administrators_controller.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+class Admin::AdministratorsController < Admin::Base
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Fbb::Application.routes.draw do
 
   root :to => 'top#index'
   resource :users
+
+  namespace :admin do
+    root to: "top#index"
+    resources :administrators
+  end
 end
