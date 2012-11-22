30da4e71912a9eafc22d51ba13aae11198872450
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-22 17:04:13 +0900

    deviseでのリダイレクト処理を変更した

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,2 +1,9 @@
 class Admin::SessionsController < ::Devise::SessionsController
+  def after_sign_in_path_for(fesource_or_scope)
+    :admin_root
+  end
+
+  def after_sign_out_path_for(fesource_or_scope)
+    :admin_root
+  end
 end
