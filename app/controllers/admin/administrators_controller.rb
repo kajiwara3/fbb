fedda1d30b897ca11fc491ba07e4a0219d3c0fe3
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-27 17:41:27 +0900

    バグを修正した

--- app/controllers/admin/administrators_controller.rb
+++ app/controllers/admin/administrators_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Admin::AdministratorsController < Admin::Base
-  before_filter :authenticate_admin_adminiatrator
+  before_filter :authenticate_admin_administrator!
   def index
     @administrators = Administrator.order(:id).page(params[:page]).per(5)
   end
