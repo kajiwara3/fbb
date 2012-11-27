10867c427f3ed814bda2e8c633e137492a95d692
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-27 17:36:59 +0900

    更新と削除アクションを実装した

--- app/controllers/admin/address_groups_controller.rb
+++ app/controllers/admin/address_groups_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Admin::AddressGroupsController < Admin::Base
   before_filter :authenticate_admin_administrator!
   def index
@@ -13,6 +14,21 @@ class Admin::AddressGroupsController < Admin::Base
   end
 
   def update
+    @address_group = AddressGroup.find(params[:id])
+    @address_group.assign_attributes(params[:address_group])
+    if @address_group.save
+      return redirect_to [:admin, @address_group], notice: '保存しました'
+    end
+    flash[:error] = '問題を確認してください'
+    return render 'edit'
+  end
 
+  def destroy
+    @address_group = AddressGroup.find(params[:id])
+    if @address_group.destroy
+      return redirect_to :admin_address_groups, notice: '削除しました'
+    end
+    flash[:error] = '削除に失敗しました'
+    return render 'show'
   end
 end
--- app/models/address_group.rb
+++ app/models/address_group.rb
@@ -1,3 +1,4 @@
 class AddressGroup < ActiveRecord::Base
   attr_accessible :address_group_code, :caption
+  validates :address_group_code, :caption, presence: true
 end
