0e9d4ce8f5f5e8265314c246389575c7a939d0a8
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-28 18:01:47 +0900

    admin/users/createを実装中

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,18 @@ class Admin::UsersController < Admin::Base
   def index
     @users = User.page(params[:page]).per(5)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.assign_attributes params[:user]
+    if @user.save
+      return redirect_to [:admin, @user], notice: I18n.t('helpers.complete.created')
+    end
+    flash[:error] = '以下の問題を確認してください'
+    return render 'new'
+  end
 end
