33e6a87c0e227b9dedba51bea06447ae38adebb8
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-11-28 17:34:57 +0900

    age_groupsコントローラーの一連の機能を実装して日本語化した

--- app/controllers/admin/age_groups_controller.rb
+++ app/controllers/admin/age_groups_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Admin::AgeGroupsController < Admin::Base
   before_filter :authenticate_admin_administrator!
   def index
@@ -6,5 +7,43 @@ class Admin::AgeGroupsController < Admin::Base
 
   def show
     @age_group = AgeGroup.find(params[:id])
+  end
+
+  def destroy
+    @age_group = AgeGroup.find(params[:id])
+    if @age_group.destroy
+      return redirect_to :admin_age_groups,
+        notice: I18n.t('helpers.complete.destroyed')
+    end
+    flash[:error] = I18n.t 'errors.template.header.one'
+    return render 'show'
+  end
+
+  def edit
+    @age_group = AgeGroup.find(params[:id])
+  end
+
+  def update
+    @age_group = AgeGroup.find params[:id]
+    @age_group.assign_attributes params[:age_group]
+    if @age_group.save
+      return redirect_to [:admin, @age_group], notice: I18n.t('helpers.complete.updated')
+    end
+    flash[:error] = I18n.t('errors.messages.template.header.one')
+    return render 'show'
+  end
+
+  def new
+    @age_group = AgeGroup.new
+  end
+
+  def create
+    @age_group = AgeGroup.new
+    @age_group.assign_attributes params[:age_group]
+    if @age_group.save
+      return redirect_to [:admin, @age_group], notice: I18n.t('helpers.complete.created')
+    end
+    flash[:error] = '以下の問題を確認してください'
+    return render 'new'
   end
 end
--- app/models/age_group.rb
+++ app/models/age_group.rb
@@ -1,3 +1,4 @@
 class AgeGroup < ActiveRecord::Base
   attr_accessible :age_group_code, :caption
+  validates :age_group_code, :caption, presence: true
 end
