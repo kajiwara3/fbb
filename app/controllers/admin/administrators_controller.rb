c7d1764a99ddfc922b266cd79a34edc95dc986d9
seiji akatsuka <info.akatsuka@gmail.com>
2013-01-24 13:54:28 +0900

    admin/administratorsの更新アクションを実装した

--- app/controllers/admin/administrators_controller.rb
+++ app/controllers/admin/administrators_controller.rb
@@ -4,4 +4,32 @@ class Admin::AdministratorsController < Admin::Base
   def index
     @administrators = Administrator.order(:id).page(params[:page]).per(5)
   end
+
+  def show
+    @administrator = Administrator.find(params[:id])
+  end
+
+  def edit
+    @administrator = Administrator.find(params[:id])
+  end
+
+  def update
+    @administrator = Administrator.find(params[:id])
+    # パスワードを更新しない場合
+    delete_password_params
+    @administrator.assign_attributes params[:administrator]
+    if @administrator.save
+      sign_in(@administrator, :bypass => true)
+      return redirect_to [:admin, @administrator], notice: '登録情報を更新しました。'
+    end
+    render 'edit'
+  end
+
+  private
+  def delete_password_params
+    if params[:administrator][:password].blank? && params[:administrator][:password_confirmation].blank?
+        params[:administrator].delete(:password)
+        params[:administrator].delete(:password_confirmation)
+    end
+  end
 end
