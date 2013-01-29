33889894eaeecafb37a26033ddedf80d08db50e1
seiji akatsuka <info.akatsuka@gmail.com>
2013-01-29 17:53:03 +0900

    スペックを追加した

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,4 +1,22 @@
 #require 'spec_helper'
 
 describe Admin::UsersController do
+  login_admin
+  subject {response}
+
+  it "should get index" do
+    get "index"
+    should be_success
+  end
+
+  it "should get new" do
+    get "new"
+    should be_success
+  end
+
+  it "should get edit form" do
+    user = FactoryGirl.create :user
+    get :edit, id: user.id
+    should be_success
+  end
 end
