1b7d1cf8f43495eee18903caf36195b013fe8114
seiji akatsuka <info.akatsuka@gmail.com>
2013-01-29 17:42:27 +0900

    スペックを追加した

--- spec/controllers/admin/address_groups_controller_spec.rb
+++ spec/controllers/admin/address_groups_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Admin::AddressGroupsController do
+  login_admin
 
+  it "should get index" do
+    get "index"
+    response.should be_success
+  end
+
+  it "should get new" do
+    get "new"
+    response.should be_success
+  end
+
+  it "should render the edit form" do
+    address_group = FactoryGirl.create :address_group
+    get :edit, id: address_group.id
+    response.should render_template 'edit'
+  end
 end
