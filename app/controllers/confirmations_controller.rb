e2f31512fcf99c9c86c03533502b7753960b8ebf
seiji akatsuka <info.akatsuka@gmail.com>
2013-01-30 12:44:00 +0900

    Devise関連のメッセージを日本語化した

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  layout false
+  # layout false
+  layout "devise_application"
   include WifiAccountManager
 
   def after_confirmation_path_for(resource_name, resource)
