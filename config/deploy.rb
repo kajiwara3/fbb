8537c191693b3f5f4434bc55b7980f03119814f3
seiji akatsuka <info.akatsuka@gmail.com>
2013-04-09 09:45:52 +0900

    stagingへのデプロイ用の設定ファイルを編集した

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,3 +51,41 @@ ssh_options[:forward_agent] = true
 set :normalize_asset_timestamps, false
 set :scm_verbose, true
 set :keep_releases, 10
+
+namespace :deploy do
+  desc "cause Passenger to initiate a restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  desc "Copy shared config files to current application."
+  task :config_symlink do
+    run "ln -s /home/fbb/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  namespace :db do
+    desc "Create #{rails_env} Database"
+    task :create do
+      puts "\n\n=== Creating the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:create RAILS_ENV=#{rails_env}"
+      system "cap deploy:set_permissions"
+    end
+  end
+end
+
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
+
+after "deploy:update_code", :bundle_install
+
+# 本コマンド実行時に、デプロイ先に配置してあるdatabase.ymlのシンボリックリンクを設定する
+after "deploy:update","deploy:config_symlink"
+# 本コマンド実行時に、デプロイ先に配置してあるdatabase.ymlのシンボリックリンクを設定する
+before "deploy:migrate", "deploy:config_symlink"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,17 +5,17 @@ set :rvm_path, '~/.rvm'
 set :rvm_bin_path, "#{rvm_path}/bin"
 set :rvm_lib_path, "#{rvm_path}/lib"
 
+#SSHユーザーの設定
+set :user, 'fbb'
+
 # デプロイ先のディレクトリ設定
 set :deploy_to, "/home/#{user}"
 # デプロイ環境名
 set :rails_env, "staging"
-server "staging-ic-vps", :app, :web, :db, primary: true
+server "staging.fbb", :app, :web, :db, primary: true
 
 # Bundle
 set :bundle_flags, ""
-
-#SSHユーザーの設定
-set :user, 'ic-vps'
 ssh_options[:keys] = %w(~/.ssh/ic-vps/rehe-ic-vps_id_rsa)
 
 namespace :deploy do
