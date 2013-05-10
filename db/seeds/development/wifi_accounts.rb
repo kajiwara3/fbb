95d4ceb3c9773190a314fd01439426e436e36afd
seiji akatsuka <info.akatsuka@gmail.com>
2013-05-10 11:49:20 +0900

    wifi_accountの初期データをstaging用の値に合わせて変更した

--- db/seeds/development/wifi_accounts.rb
+++ db/seeds/development/wifi_accounts.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 0.upto(49) do |idx|
   WifiAccount.create(
-    sign_in_code: "ssid_aabbccdd#{idx + 1}",
-    sign_in_pass: "password_#{idx + 1}"
+    sign_in_code: "fbb#{sprintf('%02d', idx + 1)}",
+    sign_in_pass: "#{sprintf('%04d', idx + 1)}"
   )
 end
