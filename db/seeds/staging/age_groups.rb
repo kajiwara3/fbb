6d69702c103bc4d2977f5b614c684f7446e6d3dc
seiji akatsuka <info.akatsuka@gmail.com>
2013-05-10 10:15:46 +0900

    staging環境の年齢情報の初期値を修正した

--- db/seeds/staging/age_groups.rb
+++ db/seeds/staging/age_groups.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-caption = ["4～12歳", "13～19歳", "20～34歳", "35～49歳", "50歳以上"]
+caption = ["12歳以下", "13～19歳", "20～34歳", "35～49歳", "50歳以上"]
 0.upto(4) do |idx|
   AgeGroup.create(
     age_group_code: idx + 1,
