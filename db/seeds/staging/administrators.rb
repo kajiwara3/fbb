dd72b7bd0604842bf6aed21cfbe02653e9cc01d6
seiji akatsuka <info.akatsuka@gmail.com>
2013-05-13 16:55:05 +0900

    管理者用の初期データを修正した

--- db/seeds/staging/administrators.rb
+++ db/seeds/staging/administrators.rb
@@ -2,11 +2,11 @@
 fnames = ["管理者 佐藤", "管理者 鈴木", "管理者 高橋", "管理者 田中"]
 gnames = ["太郎", "次郎", "花子　"]
 
-0.upto(9) do |idx|
+0.upto(3) do |idx|
   Administrator.create(
     name: "#{fnames[idx % 4]} #{gnames[idx % 3]}",
     email: "admin#{idx}@a.com",
-    password: "password",
-    password_confirmation: "password"
+    password: "$G6v#7AX",
+    password_confirmation: "$G6v#7AX"
   )
 end
