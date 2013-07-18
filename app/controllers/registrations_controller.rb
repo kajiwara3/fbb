ad20f31cf98a1b1958d4605e1b7fd06cf14cc909
seiji akatsuka <info.akatsuka@gmail.com>
2013-07-18 16:29:31 +0900

    アクティベーションメールを介さずにWi-Fiアカウント情報を設定するようにした

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+  include WifiAccountManager
+
+  def after_sign_up_path_for(resource)
+    @user = current_user
+    # wifi accountの設定
+    set_wifi_accout_to_user(@user)
+    # mail send
+    ReportMailer.wifi_password_information(@user).deliver
+    # redirect to this url
+    wellcome_path
+  end
+
   def edit
     render layout: 'application'
   end
